feccc4b0262cc8fb146999d652fe5be71453a445
teyden <teydenn@gmail.com>
2017-03-04 10:49:46 -0500

    [server setup] Add Flask Blueprint for module instantiation

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,8 @@ from flask import Flask
 
 app = Flask(__name__, static_url_path='/static')
 
-from server import server
+# Import components using blueprint handler variable
+from app.api.controller import api as api_module
+
+# Register blueprints
+app.register_blueprint(api_module) 
--- app/api/controller.py
+++ app/api/controller.py
@@ -1,49 +1,56 @@
+from flask import Blueprint
+
 from flask import session as login_session
 import random, string
 
 from flask import render_template, url_for, flash
 from flask import make_response, request, redirect, jsonify
 
-@app.route("/")
+# Import model
+# from app.api.model import Users, Stories, Adventures, Articles
+
+api = Blueprint('api', __name__, url_prefix='/')
+
+@api.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/api/stories", methods=["GET", "POST"])
+@api.route("/api/stories", methods=["GET", "POST"])
 def getStories():
   """
   Returns a list of all stories.
   """
   pass
 
-@app.route("/api/adventures", methods=["GET", "POST"])
+@api.route("/api/adventures", methods=["GET", "POST"])
 def getAdventures():
   """
   Returns a list of all adventures.
   """
   pass
 
-@app.route("/api/articles", methods=["GET", "POST"])
+@api.route("/api/articles", methods=["GET", "POST"])
 def getArticles():
   """
   Returns a list of all articles.
   """
   pass
 
-@app.route("/api/stories/<story_id>", methods=["GET"])
+@api.route("/api/stories/<story_id>", methods=["GET"])
 def getStory(article_id):
   """
   Returns a single story.
   """
   pass
 
-@app.route("/api/adventures/<adventure_id>", methods=["GET"])
+@api.route("/api/adventures/<adventure_id>", methods=["GET"])
 def getAdventure(adventure_id):
   """
   Returns a single adventure.
   """
   pass
 
-@app.route("/api/articles/<article_id>", methods=["GET"])
+@api.route("/api/articles/<article_id>", methods=["GET"])
 def getArticle(article_id):
   """
   Returns a single article.
