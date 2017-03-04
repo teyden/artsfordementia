692256542eff9db0bf7d14796a82ec31d45a606e
teyden <teydenn@gmail.com>
2017-03-03 23:42:50 -0500

    Remove database hooks

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
 from flask import Flask
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import User, Story, Adventure, Article
-
 from flask import session as login_session
 import random, string
 
 from flask import render_template, url_for, flash
 from flask import make_response, request, redirect, jsonify
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-import httplib2, json, requests 
 
 app = Flask(__name__)
-
-APPLICATION_NAME = "Arts for Dementia"
-
-## Connect to DB and create DB session
-engine = create_engine('sqlite:///data/db/afd.db')
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/api/stories", methods=["GET"])
+@app.route("/api/stories", methods=["GET", "POST"])
 def getStories():
   """
   Returns a list of all stories.
   """
   pass
 
-@app.route("/api/adventures", methods=["GET"])
+@app.route("/api/adventures", methods=["GET", "POST"])
 def getAdventures():
   """
   Returns a list of all adventures.
   """
   pass
 
-@app.route("/api/articles", methods=["GET"])
+@app.route("/api/articles", methods=["GET", "POST"])
 def getArticles():
   """
   Returns a list of all articles.
