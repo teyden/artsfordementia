50300293a24dedd699fbd6e026e7c9e25fb8206b
teyden <teydenn@gmail.com>
2017-03-04 22:20:38 -0500

    Implement REST endpoints (GET and POST) for /api/adventures

--- app/api/controller.py
+++ app/api/controller.py
@@ -3,13 +3,17 @@ from flask import Blueprint
 from flask import session as login_session
 import random, string
 
+from datetime import datetime
+
 from flask import render_template, url_for, flash
 from flask import make_response, request, redirect, jsonify, send_from_directory
 
 # Import model
 from app.api.model import db
+from app.api.model import STORY, ADVENTURE, ARTICLE
+from app.api.model import STORY_SEED, ADVENTURE_SEED, ARTICLE_SEED
 
-api = Blueprint('api', __name__, url_prefix='/')
+api = Blueprint('api', __name__, url_prefix='')
 
 @api.route("/")
 def index():
@@ -20,25 +24,142 @@ def index():
 def send_images(path):
   return send_from_directory('images', path)
 
+# Login and signup 
+
+
+# Helpers
+def alreadyExists(object, type):
+  """
+  Checks the DB for whether the object already exists, using the external id
+  and type.
+  """
+  isStory = type == STORY and db.StoryCollection.find({'id': { "$in": object[id]}}).count() > 0
+  isAdventure = type == ADVENTURE and db.AdventureCollection.find({'id': { "$in": object[id]}}).count() > 0
+  isArticle = type == ARTICLE and db.ArticleCollection.find({'id': { "$in": object[id]}}).count() > 0
+  
+  if isStory or isAdventure or isArticle:
+    return True
+  else:
+    return False
+
+def isValidSeed(json, seed):
+  for key in seed.keys():
+    if key not in json:
+      return False 
+  return True
+
+def isValidJsonObject(json, type):
+  if type == STORY:
+    return isValidSeed(json, STORY_SEED)
+  elif type == ADVENTURE:
+    return isValidSeed(json, ADVENTURE_SEED)
+  elif type == ARTICLE:
+    return isValidSeed(json, ARTICLE_SEED)
+  return False
+
+def getFormattedJson(json, seed):
+  outputJson = {}
+  for key in seed:
+    outputJson[key] = json[key]
+  return outputJson 
+
+def makeJsonObjectProperFormat(json, type):
+  """
+  The json object entered must have all the fields for the associated
+  json seed type.
+  """
+  if type == STORY:
+    return getFormattedJson(json, STORY_SEED)
+  elif type == ADVENTURE:
+    return getFormattedJson(json, ADVENTURE_SEED)
+  elif type == ARTICLE:
+    return getFormattedJson(json, ARTICLE_SEED)
+  else:
+    print "Error in formatting json"
+    return {}
+
 # REST endpoints
 @api.route("/api/stories", methods=["GET", "POST"])
-def getStories():
+def stories():
   """
-  Returns a list of all stories.
+  GET - returns a list of all stories.
+  POST - creates a story provided as a JSON object
+
+  Example request:
+    {
+      "title": "Test Story",
+      "description": "A test description",
+      "image": "",
+      "media_link": "https://youtube.com/somevideo",
+      "user_email": "afakeemailaddress@gmail.com"
+    }
   """
-  pass
+  if request.method == "GET":
+    stories = []
+    for story in db.StoryCollection.find():
+      del story['_id']
+      stories += [story]
+    return jsonify(stories=stories)
+
+  elif request.method == "POST":
+    json = request.get_json()
+    if isValidJsonObject(json, STORY):
+      storyJson = makeJsonObjectProperFormat(json, STORY)
+      storyJson['upload_date'] = datetime.today()
+      db.StoryCollection.insert(storyJson)
+      return make_response('Story successfully created.', 201)
+    return make_response('Error with resource.', 409)
+  return make_response('Method not allowed.', 405)
+
 
 @api.route("/api/adventures", methods=["GET", "POST"])
-def getAdventures():
+def adventures():
   """
-  Returns a list of all adventures.
+  GET - returns a list of all adventures.
+  POST - creates an adventure provided as a JSON object
+
+  Example request:
+    {
+      "title": "Run for Fun",
+      "category": "Exercise",
+      "name_of_place": "Some name",
+      "address": "483 Evergreen Street",
+      "event_date": "",
+      "event_time": "", 
+      "description": "Some adventure description.",
+      "tags": ["active","running","onyourfeet"],
+      "user_email": "afakeemailaddress@gmail.com"
+    }
   """
-  pass
+  if request.method == "GET":
+    adventures = []
+    for adventure in db.AdventureCollection.find():
+      del adventure['_id']
+      adventures += [adventure]
+    return jsonify(adventures=adventures)
+
+  elif request.method == "POST":
+    json = request.get_json()
+    if isValidJsonObject(json, ADVENTURE):
+      adventureJson = makeJsonObjectProperFormat(json, ADVENTURE)
+      adventureJson['upload_date'] = datetime.today()
+      db.AdventureCollection.insert(adventureJson)
+      return make_response('Adventure successfully created.', 201)
+    return make_response('Error with resource.', 409)
+  return make_response('Method not allowed.', 405)
 
 @api.route("/api/articles", methods=["GET", "POST"])
-def getArticles():
+def articles():
   """
-  Returns a list of all articles.
+  GET - returns a list of all articles.
+  POST - creates an article provided as a JSON object
+
+  Example request:
+    {
+      "title": "An article for clinicians",
+      "content": "Paragraphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blaphs of content blah blah blah blah",
+      "user_email": "afakeemailaddress@gmail.com"
+    }
   """
   pass
 
@@ -62,3 +183,5 @@ def getArticle(article_id):
   Returns a single article.
   """
   pass
+
+# Upload method that handles two kinds of parameters
--- app/api/model.py
+++ app/api/model.py
@@ -6,4 +6,30 @@ MONGO_DATABASE_STR = MONGOLAB_URI.split('/')[-1]
 client = MongoClient(MONGOLAB_URI)
 db = client[MONGO_DATABASE_STR]
 
-TestCollection = db.TestCollection
+STORY = "story"
+ADVENTURE = "adventure"
+ARTICLE = "article"
+
+# Data seeds
+STORY_SEED = {}
+STORY_SEED["title"] = ""
+STORY_SEED["description"] = ""
+STORY_SEED["image"] = ""
+STORY_SEED["media_link"] = ""
+STORY_SEED["user_email"] = ""
+
+ADVENTURE_SEED = {}
+ADVENTURE_SEED["title"] = ""
+ADVENTURE_SEED["category"] = ""
+ADVENTURE_SEED["name_of_place"] = ""
+ADVENTURE_SEED["address"] = ""
+ADVENTURE_SEED["event_date"] = ""
+ADVENTURE_SEED["event_time"] = ""
+ADVENTURE_SEED["description"] = ""
+ADVENTURE_SEED["tags"] = []
+ADVENTURE_SEED["user_email"] = ""
+
+ARTICLE_SEED = {}
+ARTICLE_SEED["title"] = ""
+ARTICLE_SEED["content"] = ""
+ARTICLE_SEED["user_email"] = ""
