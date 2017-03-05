455bfac6d37a566e461a75e9cfdf59f91c453d57
guptamo <mo@mohitgupta.ca>
2017-03-05 01:44:18 -0500

    Basic grid implementation complete. Not yet responsive

--- app/ui/components/home.js
+++ app/ui/components/home.js
@@ -7,7 +7,10 @@ import {
     ToolbarTitle,
     Avatar,
     ListItem,
-    FontIcon} from 'material-ui'
+    FontIcon,
+    TextField,
+    GridList,
+    GridTile} from 'material-ui'
 import ResponsiveEmbed from 'react-responsive-embed'
 
 import StoryDialog from './story-dialog'
@@ -21,8 +24,67 @@ const pinnedButtonStyle = {
     "position": "fixed",
     "bottom": "5%",
     "right": "5%"
-
 }
+
+const searchbarStyle = {
+    "marginTop": "3em"
+}
+
+const gridListStyle = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        marginTop: '3em'
+    },
+    gridStyle: {
+        width: 960,
+        overflowY: 'auto'
+    }
+}
+
+const tilesData = [
+  {
+    img: '',
+    title: 'Breakfast',
+    author: 'jill111',
+  },
+  {
+    img: '',
+    title: 'Tasty burger',
+    author: 'pashminu',
+  },
+  {
+    img: '',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: '',
+    title: 'Morning',
+    author: 'fancycrave1',
+  },
+  {
+    img: '',
+    title: 'Hats',
+    author: 'Hans',
+  },
+  {
+    img: '',
+    title: 'Honey',
+    author: 'fancycravel',
+  },
+  {
+    img: '',
+    title: 'Vegetables',
+    author: 'jill111',
+  },
+  {
+    img: '',
+    title: 'Water plant',
+    author: 'BkrmadtyaKarki',
+  },
+];
 
 class Home extends React.Component {
     constructor(props){
@@ -57,6 +119,21 @@ class Home extends React.Component {
                 <ResponsiveEmbed src="https://www.youtube.com/embed/ejhnvrOn2gI" allowFullScreen/>
                 <Toolbar><ToolbarTitle text="Insert Title Here"></ToolbarTitle></Toolbar>
                 <main style={mainStyle}>
+                    <TextField fullWidth={true} id="search-bar" style={searchbarStyle} floatingLabelText="Search"/>
+                    <div style={gridListStyle.root}>
+                        <GridList
+                            style={gridListStyle.gridStyle}
+                            cols={3}
+                            padding={8}
+                            cellHeight={233}>
+                            {tilesData.map((tile) => (
+                                <GridTile
+                                    key={tile.title}
+                                    title={tile.title}
+                                    subtitle={tile.author}></GridTile>
+                            ))}
+                        </GridList>
+                    </div>
                     <FloatingActionButton iconClassName="fa fa-plus" onClick={this.openDialog} style={pinnedButtonStyle}/>
                     <StoryDialog open={this.state.open} closeDialog={this.closeDialog} />
                 </main>
